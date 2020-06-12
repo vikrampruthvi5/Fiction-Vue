3a72cb101a214d7f9e3594555941b844f499efeb
Pruthvi Vikram <vikrampruthvi5@gmail.com>
2020-06-12 08:13:35 +0200

    captcha completed: tested successfully

---
+++ src/components/Navigation.vue
@@ -0,0 +1,23 @@
+<template>
+    <v-toolbar dense dark>
+      <!-- <v-app-bar-nav-icon></v-app-bar-nav-icon> -->
+
+      <v-toolbar-title>
+          <span class="grey--text">SIMFIC 2.0</span>
+      </v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+    </v-toolbar>
+</template>
+
+
+<script>
+    export default {
+        name: "Navigation"
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
